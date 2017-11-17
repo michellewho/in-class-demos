09ee6f072e1590d54a22265fa64227f01e71d06d
Michelle <michho@uw.edu>
2017-11-17 10:39:15 -0800

    updated react-state demo

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -1,21 +1,70 @@
 import React, { Component } from 'react';
 
+import 'whatwg-fetch'; //load polyfill
+
 const SAMPLE_TASKS = [
-  {id:1, description:'Learn JSX', complete:true},
-  {id:2, description:'Learn about React State', complete:false},
-  {id:3, description:'Get some sleep', complete:false} 
+  { id: 1, description: 'Learn JSX', complete: true },
+  { id: 2, description: 'Learn about React State', complete: false },
+  { id: 3, description: 'Get some sleep', complete: false }
 ];
 
 
 class App extends Component {
 
+  constructor(props) {
+    // set up
+    super(props)
+
+    // initial state values
+    // state is always an object
+    this.state = {
+      currentTime: new Date(),
+      incompleteCount: 'A lot',
+      tasks: SAMPLE_TASKS
+    };
+
+    window.setInterval(() => {
+
+      let updatedState = { currentTime: new Date() }
+      this.setState(updatedState);
+
+    }, 1000);
+  }
+
+  componentDidMount() {
+    fetch('tasks.json')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) =>{
+        this.setState({tasks.data})
+      })
+  }
+
+  // toggleFinished(taskId) {
+  //   let updatedTasks = this.state.tasks.map((task) => {
+  //     if(task.id === task)
+  //   })
+  // }
+
+  addTask(description) {
+    let newTask = {
+      id:this.state.tasks.length,
+      description:description,
+      complete: false
+    }
+
+    let updatedTasks = this.state.tasks.concat(newTask);
+    this.setState({tasks:updatedTasks});
+  }
+
   render() {
     return (
       <div className="container">
-        <Clock />
+        <Clock time={this.state.currentTime} />
         <p className="lead">Things I have to do (2+)</p>
         <TaskList />
-        <AddTaskForm />
+        <AddTaskForm submitCallback = {(descr) => {this.AddTask(descr)}}/>
       </div>
     );
   }
@@ -24,17 +73,47 @@ class App extends Component {
 class Clock extends Component {
   render() {
     return (
-        <div className="font-italic">{"demo o'clock"}</div>
-    );  
+      <div className="font-italic">{"demo o'clock"}</div>
+    );
   }
 }
 
-class TaskList extends Component {  
-  render() {   
+class TaskList extends Component {
+  constructor(props) {
+    super(props);
+
+    //initial values 
+    this.state = {
+      tasks: SAMPLE_TASKS
+    }
+  }
+
+  toggleFinished(taskId) {
+    let updatedTasks = this.props.tasks.map((task) => {
+      if (task.id === taskId) {
+        task.complete = !task.complete; //toggle
+      }
+      return task;
+    })
+
+    this.setState({ tasks: updatedTasks });
+  }
+
+  render() {
+    // [{}, {}, {}] => [<Task> <Task> <Task]
+
+    let taskItemsArray = this.state.tasks.map((task) => {
+      return <Task
+        key={task.id}
+        task={task}
+        toggleCallback={() => { this.toggleFinished(taskId) }}
+      />;
+
+    })
+
     return (
       <ol>
-        <Task />
-        {/* tasks go here */}
+        {taskItemsArray}
       </ol>
     );
   }
@@ -42,25 +121,49 @@ class TaskList extends Component {
 
 class Task extends Component {
 
+  handleClick() {
+    console.logo("You clicked on #", this.props.task.id);
+    this.props.toggleCallback(this.props.task.id);
+  }
+
   render() {
 
+    let className = this.props.task.complete ? 'font-strike' : '';
+
     return (
-      <li>
-        task description
+      <li className={className} onClick={() => { this.handleClick() }}>
+        {this.props.task.description}
       </li>
     );
   }
 }
 
 class AddTaskForm extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { value: '' }
+
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value, () => {
+      console.log(this.state.value);
+    }})
+  }
+
   render() {
     return (
       <form>
-        <input 
+        <input
           className="form-control mb-3"
           placeholder="What else do you have to do?"
-          />
-        <button className="btn btn-primary">Add task to list</button>
+          value={this.state.value}
+          onChange={(event) => { this.handleChange(event) }}
+        />
+        <button className="btn btn-primary" onClick= {(event) => {this.handleClick(event)}}
+          >Add task to list</button>
       </form>
     );
   }
